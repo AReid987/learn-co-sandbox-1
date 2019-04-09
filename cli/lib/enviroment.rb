f710ce55b9f5740044edf682480b2eeeec87212c
lieby weintraub <liebahinda@gmail.com>
2019-04-09 22:15:18 +0000

    Automatically backed up by Learn

--- cli/lib/enviroment.rb
+++ cli/lib/enviroment.rb
@@ -3,6 +3,9 @@ require_relitive "./cli/version"
 require_relitive "./cli/enviroment"
 require_relitive "./cli/library"
 
+require "./nokogiri"
+require "pry"
+
 module Cli
   class Error < StandardError; end
   # Your code goes here...
