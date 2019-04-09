5989e6f4472593a2405f701edaa8bfef88a002aa
lieby weintraub <liebahinda@gmail.com>
2019-04-09 22:25:24 +0000

    Automatically backed up by Learn

--- cli/lib/enviroment.rb
+++ cli/lib/enviroment.rb
@@ -1,11 +1,14 @@
 #require "cli/version"
+require "nokogiri"
+require "open-uri"
+require "pry"
+
+
 require_relitive "./cli/version"
 require_relitive "./cli/enviroment"
 require_relitive "./cli/library"
 
-require "nokogiri"
-require "open-uri"
-require "pry"
+
 
 module Cli
   class Error < StandardError; end
